412682e07f8a1e4a690a70208323c2417a057a05
Trevor Vaughan <tvaughan@onyxpoint.com>
2017-06-07 22:02:04 -0400

    working as expected

--- spec/acceptance/suites/compliance/10_inspec_spec.rb
+++ spec/acceptance/suites/compliance/10_inspec_spec.rb
@@ -5,67 +5,122 @@ test_name 'Validate Compliance'
 
 describe 'use inspec to check compliance' do
 
-  package_matrix = {
-    'RedHat' => {
-      '6' => {
-        'package' => 'https://packages.chef.io/files/stable/inspec/1.25.1/el/6/inspec-1.25.1-1.el6.x86_64.rpm'
-      },
-      '7' => {
-        'package' => 'https://packages.chef.io/files/stable/inspec/1.25.1/el/7/inspec-1.25.1-1.el7.x86_64.rpm'
-      }
-    },
-    'CentOS' => {
-      '6' => {
-        'package' => 'https://packages.chef.io/files/stable/inspec/1.25.1/el/6/inspec-1.25.1-1.el6.x86_64.rpm'
-      },
-      '7' => {
-        'package' => 'https://packages.chef.io/files/stable/inspec/1.25.1/el/7/inspec-1.25.1-1.el7.x86_64.rpm'
-      }
-    }
-  }
-
   profiles_to_validate = ['disa_stig']
 
   profile_dir = '/tmp/inspec_tests'
+  output_dir  = File.absolute_path('inspec_results')
+  timestamp   = Time.now.to_i
+
+  def process_inspec_results(inspec_results)
+    require 'highline'
+
+    HighLine.colorize_strings
+
+    stats = {
+      :passed => 0,
+      :failed => 0,
+      :report => []
+    }
+
+    profiles = inspec_results['profiles']
+
+    profiles.each do |profile|
+      stats[:report] << "Name: #{profile['name']}"
+
+      profile['controls'].each do |control|
+        title = control['title']
+        if title.length > 72
+          title = title[0..71] + '(...)'
+        end
+
+        title_chunks = control['title'].scan(/.{1,72}\W|.{1,72}/).map(&:strip)
+
+        stats[:report] << "\n  Control: #{title_chunks.shift}"
+        unless title_chunks.empty?
+          title_chunks.map!{|x| x = "           #{x}"}
+          stats[:report] << title_chunks.join("\n")
+        end
+
+        status = control['results'].first['status']
+
+        status_str = "    Status: "
+        if status =~ /^fail/
+          stats[:failed] += 1
+
+          stats[:report] << status_str + status.red
+          stats[:report] << "    File: #{control['source_location']['ref']}"
+        else
+          stats[:passed] += 1
+
+          stats[:report] << status_str + status.green
+        end
+      end
+
+      stats[:report] << "\n  Statistics:"
+      stats[:report] << "    * Passed: #{stats[:passed].to_s.green}"
+      stats[:report] << "    * Failed: #{stats[:failed].to_s.red}"
+      stats[:report] << "    * Score:  #{((stats[:passed].to_f/(stats[:passed] + stats[:failed])) * 100.0).round(0)}%"
+    end
+
+    stats[:report] = stats[:report].join("\n")
+
+    return stats
+  end
+
+  before(:each) do
+    unless File.directory?(output_dir)
+      FileUtils.mkdir(output_dir)
+    end
+  end
 
   hosts.each do |host|
     profiles_to_validate.each do |profile|
       context "for profile #{profile}" do
 
-        let(:timestamp) { Time.now.to_i }
+        let(:inspec_results) { File.join(output_dir, "#{host.hostname}-inspec-#{timestamp}") }
 
         context "on #{host}" do
           os = fact_on(host, 'operatingsystem')
           os_rel = fact_on(host, 'operatingsystemmajrelease')
 
-          package_matrix.keys.each do |operatingsystem|
-            if os == operatingsystem
-              context "on #{operatingsystem}" do
-                package_matrix[operatingsystem].keys.each do |release|
-                  if os_rel == release
-                    it 'should have inspec installed' do
-                      host.install_package(package_matrix[operatingsystem][release]['package'])
-                    end
+          it 'should have inspec installed' do
+            host.install_package('inspec')
+          end
 
-                    it "should copy over the #{profile} inspec profile" do
-                      scp_to(host, "spec/fixtures/inspec/#{operatingsystem}_#{release}_#{profile}", profile_dir)
-                    end
+          it "should copy over the #{profile} inspec profile" do
+            scp_to(host, "spec/fixtures/inspec/#{os}_#{os_rel}_#{profile}", profile_dir)
+          end
 
-                    it 'should run inspec and export results' do
-                      json_output = on(host, "inspec exec --format json #{profile_dir}", :silent => true).stdout.strip
+          it 'should run inspec and export results' do
+            on(host, "inspec exec --format json #{profile_dir} > /tmp/inspec_results.json", :silent => true)
 
-                      # Strip off garbage
-                      json_output = json_output[json_output.index(/{"/)..-1]
+            tmpdir = Dir.mktmpdir
+            begin
+              Dir.chdir(tmpdir) do
+                scp_from(host, '/tmp/inspec_results.json', '.')
 
-                      File.open("inspec_json-#{timestamp}.log", 'w') { |file|
-                        #file.write(JSON.pretty_generate(json_output))
-                        file.write(json_output)
-                      }
-                    end
-                  end
+                File.open(inspec_results + '.json', 'w') do |fh|
+                  inspec_json = JSON.load(File.read('inspec_results.json'))
+                  fh.puts(JSON.pretty_generate(inspec_json))
                 end
               end
+            ensure
+              FileUtils.remove_entry_secure tmpdir
             end
+          end
+
+          it 'should not have any failing tests' do
+            inspec_report = process_inspec_results(JSON.load(File.read(inspec_results + '.json')))
+
+            File.open(inspec_results + '.report', 'w') do |fh|
+              fh.puts(inspec_report[:report].uncolor)
+            end
+
+            if inspec_report[:failed] > 0
+              puts inspec_report[:report]
+            end
+
+            expect( inspec_report[:failed] ).to eq(0)
           end
         end
       end
