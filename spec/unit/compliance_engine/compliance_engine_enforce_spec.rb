1bf63c13c0e653f0833f86c13f3008b286383398
Brandon Riden <brandon.riden@onyxpoint.com>
2019-06-18 20:00:51 -0400

    Update fixtures and enfore_spec test

--- spec/unit/compliance_engine/compliance_engine_enforce_spec.rb
+++ spec/unit/compliance_engine/compliance_engine_enforce_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
 
+# Remove v1 data. Can be removed once compliance_markup::debug::enabled_sce_versions is implemented
+v1_profiles = './spec/fixtures/modules/compliance_markup/data/compliance_profiles'
+FileUtils.rm_rf(v1_profiles) if File.directory?(v1_profiles)
+
 # This is the class that needs to be added to the catalog last to make the
 # reporting work.
 describe 'compliance_markup', type: :class do
 
   compliance_profiles = [
     'disa_stig',
-    'nist_800_53',
     'nist_800_53_rev4'
   ]
 
@@ -15,13 +18,18 @@ describe 'compliance_markup', type: :class do
   # This needs to be well defined since we can also manipulate defined type
   # defaults
   expected_classes = [
-    'auditd'
+    'auditd',
   ]
+
+  allowed_failures = {
+    'documented_missing_parameters' => [],
+    'documented_missing_resources' => []
+  }
 
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
+      let(:hieradata){ 'compliance-engine' }
       let(:facts){ os_facts }
-      let(:node){ "compliance-test" }
 
       compliance_profiles.each do |target_profile|
         context "with compliance profile '#{target_profile}'" do
@@ -43,6 +51,10 @@ describe 'compliance_markup', type: :class do
             expect(compliance_profile_data).to_not be_nil
           end
 
+          it 'should have a 100% compliant report' do
+            expect(compliance_profile_data['summary']['percent_compliant']).to eq(100)
+          end
+
           # The list of report sections that should not exist and if they do
           # exist, we need to know what is wrong so that we can fix them
           report_validators = [
@@ -50,16 +62,25 @@ describe 'compliance_markup', type: :class do
             'non_compliant',
             # If something is set here, either the upstream API changed or you
             # have a typo in your data
-            'documented_missing_parameters'
+            'documented_missing_parameters',
+            # If something is set here, you have included enforcement data that
+            # you are not testing so you either need to remove it from your
+            # profile or you need to add the class/defined type for validation
+            #
+            # Unless this is a completely comprehensive data profile, with all
+            # classes included, this report may be useless and is disabled by
+            # default.
+            #
+            'documented_missing_resources'
           ]
 
           report_validators.each do |report_section|
             it "should have no issues with the '#{report_section}' report" do
               if compliance_profile_data[report_section]
                 # This just gets us a good print out of what went wrong
-                expect(compliance_profile_data[report_section]).to eq({})
-              else
-                expect(compliance_profile_data[report_section]).to be_nil
+                expect(
+                  compliance_profile_data[report_section] - Array(allowed_failures[report_section])
+                ).to eq([])
               end
             end
           end
